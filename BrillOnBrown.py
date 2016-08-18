d1c20eca81b978b624bb1ed09f1a8c206b15de08
Byung-Chul In <b1in@ucsd.edu>
2016-08-17 21:14:26 -0700

    Have to training Brown corpus..

--- BrillOnBrown.py
+++ BrillOnBrown.py
@@ -1,9 +1,71 @@
-from nltk.tbl.template import template  # <- ??
+'''
+	I want to train a Brill's Tagger on Brown Corpus
+	
+	Brill's tagger is a TBL (transformation based Learning) approach
+	It is based on rules that specify what tag should be assigned
+	to what words. It is a supervised technique; it assumes there is
+	pre-tagged training corpus.
+'''
+
+from nltk.tbl.template import Template  # <- ??
 from nltk.tag.brill import Pos, Word
-from nltk.tag import RegexpTagger
-from nltk.tag.brill_trainer_orig import BrillTaggerTrainer
+from nltk.tag import RegexpTagger, untag
+from nltk.tag.brill_trainer import BrillTaggerTrainer
 from nltk.corpus import brown
 
-training_data = brown.tagged_sents(categories=['news'])[:100]
-baseline_data = brown.tagged_sents(categories=['news'])[100:200]
-gold_data = brown.tagged_sents(categories=['news'])[200:300]
+training_data = brown.tagged_sents(categories=['news'])[:1000]
+baseline_data = brown.tagged_sents(categories=['news'])[1000:2000]
+gold_data = brown.tagged_sents(categories=['news'])[2000:3000]
+testing_data = [untag(s) for s in gold_data]
+
+'''
+	backoff tagger is needed when the program face with the 
+	oov (out of vocabulary) words. So instead of tag them as 
+	None, backoff tagger can help reduce problem.
+
+	We will use some Regexp pattern for the back off
+'''
+
+backoff = RegexpTagger([
+		(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),  # cardinal numbers
+		(r'(The|the|A|a|An|an)$','AT'),  # articles
+		(r'.*ables$', 'JJ'),  # adjectives
+		(r'.*ness$', 'NN'),  # nounds formed from adjectives
+		(r'.*ly$', 'RB'),  # adverbs
+		(r'.*s$','NNS'),  # plural nouns
+		(r'.*ing$','VBG'),  # gerunds
+		(r'.*ed$', 'VBD'),  # past tense verbs
+		(r'.*', 'NN')	# nouns (default)
+		])
+
+baseline = backoff   # better baseline uses Unigram tagger (??)
+
+'''
+	some suggested rules for the template
+		change the POS of a word, depending on the POS of the previous word
+		change the POS of a word, depending on the POS of any of the two previous words
+		change the POS of a word, depending on the POS of any of the three previous words
+		change the POS of a word, depending on the POS of the previous word and the POS of the next word
+		change the POS of a word, depending on the previous word
+		change the POS of a word, depending on any of the two previous words
+		change the POS of a word, depending on any of the three previous words
+		change the POS of a word, depending on the previous word and the next word
+'''
+
+# construct template
+Template._cleartemplates()  # clear any templates created in eariler tests
+
+templates = [Template(Pos([-1])), Template(Pos([-1]), Word([0]))]
+
+# construct a Brill Tagger Trainer
+'''
+	BrillTaggerTrainer(1st, 2nd, 3rd, ...)
+		1st param initial_tagger: (Tagger) the baseline tagger
+		2nd param templates : (list of templates) templates to be used in training
+		3rd param trace: (int) verbosity level == information level u want to see
+		4th param deterministic: (bool) if True, adjudicate ties deterministically
+		5th ruleformat: (str) format of reported rules
+'''
+ret = BrillTaggerTrainer(baseline, templates, trace=3)
+
+tagger1 = ret.train(training_data, max_rules=10)
